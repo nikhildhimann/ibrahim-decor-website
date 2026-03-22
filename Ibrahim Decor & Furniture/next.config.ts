8d0a3afa20a8032975b6866ce31694fe5ddb900e
nikhildhimann <nikhildhimaann@gmail.com>
2026-03-22 12:50:24 +0530

    update

--- Ibrahim Decor & Furniture/next.config.ts
+++ Ibrahim Decor & Furniture/next.config.ts
@@ -6,16 +6,7 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
+
 
 export default withNextIntl(nextConfig);
